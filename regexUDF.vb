0cec51e15e030a3b16c28ec9bd691f974dcf4835
Sage Hourihan <samiho97@gmail.com>
2023-09-14 14:43:21 -0400

    Update regexUDF.vb
    updated to not need double quotes

--- regexUDF.vb
+++ regexUDF.vb
@@ -1,35 +1,32 @@
-'-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-'Code to make a user defined regex function in excel. Must turn on Regex in Visual Basic -> Tools -> References. Check Microsoft 'VBScript Regular Expressions 1.0 and Microsoft VBScript Regular Expressions 5.5
-'-----------------------------------------
-'code used from: https://stackoverflow.com/questions/22542834/how-to-use-regular-expressions-regex-in-microsoft-excel-both-in-cell-and-loops
-'syntax listed below
-'
-'
-'=regex(cell number, "regex")
-'----------------------------------------------------------------
-'creating fucntion name and variables
-'strInput = cell you want to run the regex command on
-'regexPattern = rexex pattern
-Function RegX(strInput As String, regexPattern As String) As String
-    'creating regEx variable as object of RegExp
-    Dim regEx As New RegExp
-    'adding with statement to execute series of statements to a single object
-    With regEx
-    'setting this function to module
-        .Global = True
-        '.MultiLine = True
-    'making pattern case sensitive
-        .IgnoreCase = False
-    'returning pattern that matches regexPattern
-        .Pattern = regexPattern
-    'ending with statement
-    End With
-
-    If regEx.Test(strInput) Then
-        Set matches = regEx.Execute(strInput)
-        RegX = matches(0).Value
+Function RegX(strInput As String, regexPattern As Variant) As String
+    ' Check if regexPattern is not empty
+    If Not IsEmpty(regexPattern) Then
+        ' Convert regexPattern to a string
+        Dim pattern As String
+        pattern = CStr(regexPattern)
+        
+        ' Create a RegExp object
+        Dim regEx As Object
+        Set regEx = CreateObject("VBScript.RegExp")
+        
+        ' Configure the RegExp object
+        With regEx
+            .Global = True
+            .IgnoreCase = False
+            .Pattern = pattern
+        End With
+        
+        ' Test the input string against the regex pattern
+        If regEx.Test(strInput) Then
+            ' If a match is found, return the matched value
+            Set matches = regEx.Execute(strInput)
+            RegX = matches(0).Value
+        Else
+            ' If no match is found, return "not matched"
+            RegX = "not matched"
+        End If
     Else
-        RegX = "not matched"
+        ' If regexPattern is empty, return an error message
+        RegX = "Regex pattern is empty"
     End If
 End Function
-
